8eacda2af72f9a331a6b9f464d166047c05b42a1
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2023-10-03 11:39:37 +0200

    workshop wilders

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Tristan"));
+console.log(chalk.red("Jonathan"));
+console.log(chalk.yellow("Raphael"));
+console.log(chalk.green("Tonton Bapt"));
